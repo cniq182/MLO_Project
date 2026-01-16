340cb1c3f2bf03b6dbc5632700320486c3ef98b4
sarahtauro <sarahtauro2603@gmail.com>
2026-01-16 15:51:25 +0100

    Update test_model.py

---
+++ en_es_translation/tests/test_model.py
@@ -0,0 +1,8 @@
+from torch.utils.data import Dataset
+from en_es_translation.data import MyDataset
+
+
+def test_my_dataset():
+    """Test the MyDataset class."""
+    dataset = MyDataset("data/raw")
+    assert isinstance(dataset, Dataset)
