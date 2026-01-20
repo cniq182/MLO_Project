3d5b4004993e3c653cdbf5f7eaf3aa51d24c7828
sarahtauro <sarahtauro2603@gmail.com>
2026-01-20 12:51:57 +0100

    add sweep

---
+++ en_es_translation/src/en_es_translation/my_logger.py
@@ -0,0 +1,14 @@
+from loguru import logger
+import sys
+
+logger.remove()
+
+# Terminal: warnings+
+logger.add(sys.stderr, level="WARNING")
+
+# File: everything
+logger.add(
+    "my_log.log",
+    level="DEBUG",
+    rotation="100 MB"
+)
